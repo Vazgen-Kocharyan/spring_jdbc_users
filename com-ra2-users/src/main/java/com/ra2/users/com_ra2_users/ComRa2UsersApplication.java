47829723cf9579aea655ba96dbe1612bd764d036
Vazgen <2024_vazgen.kocharyan@iticbcn.cat>
2025-10-22 23:51:19 +0200

    project started

---
+++ com-ra2-users/src/main/java/com/ra2/users/com_ra2_users/ComRa2UsersApplication.java
@@ -0,0 +1,13 @@
+package com.ra2.users.com_ra2_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComRa2UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComRa2UsersApplication.class, args);
+	}
+
+}
---
+++ com-ra2-users/src/test/java/com/ra2/users/com_ra2_users/ComRa2UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ra2.users.com_ra2_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComRa2UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
